d53208e28eb61bb1bd03c1f3afaa008cb1e9667e
tokhi <shafi.tokhi@gmail.com>
2014-03-04 16:44:09 +0000

    rails 4 angular js + twitter bootstrap setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'e184f7707f1c2ba7f7999d3661d82e1e4083887e4ab7d15f464091963c62699570efcbdae0bfe685f8f425e2700cb2b71086f60790d7867fe747924aff820981'
